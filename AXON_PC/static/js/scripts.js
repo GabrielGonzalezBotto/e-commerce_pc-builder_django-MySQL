4d7d49af7ca8ec482d124ec93ec0262efea76a0f
Gabriel <gabrielgonzalezbotto@gmail.com>
2026-09-10 13:54:14 -0300

    feat: Botón claro/oscuro

---
+++ AXON_PC/static/js/scripts.js
@@ -0,0 +1,20 @@
+//Botón Claro / Oscuro//
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("toggle-theme");
+    const html = document.documentElement;
+
+    //Recupera el tema guardado en localStorage
+    const savedTheme = localStorage.getItem("theme");
+    if(savedTheme) {
+        html.setAttribute("data-bs-theme", savedTheme);
+    }
+
+    btn.addEventListener("click", function() {
+        const current = html.getAttribute("data-bs-theme");
+        const next = current === "dark" ? "light" : "dark";
+        html.setAttribute("data-bs-theme", next);
+
+        //Guarda el tema elegido en localStorage
+        localStorage.setItem("theme", next)
+    });
+});
